52b54963cf52f4ecf2f55e2f504125e0c934b6b1
hemanth.hm <hemanth.hm@gmail.com>
2012-05-26 22:20:45 +0530

    Using the API now

--- lib/hackersays.rb
+++ lib/hackersays.rb
@@ -1,14 +1,13 @@
 #!/usr/bin/env ruby
-require 'yaml'
+require 'json'
 require 'open-uri'
+
 class Hacker
   def self.said
-    @quotes = []
-    hackers_said = File.dirname(__FILE__) + "/.said"
-    @quotes = YAML::load(open(hackers_said).read)
-    quote = @quotes[rand(@quotes.length)]
-    said  = quote[:c]
-    who   = quote[:a] || "Some hacker"
-    "#{said} ~ #{who}".gsub(/\n/,"")
+    quote_url = "http://hackersays.com/quote"
+    quote = JSON::load(open(quote_url).read())
+    comment  = quote["c"]
+    author = quote["a"] || "Some hacker"
+    "#{comment} ~ #{author}"
   end
 end
